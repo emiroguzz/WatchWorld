b29dfd51e5b9f0741f9756b6ad04ee3f043f6fbe
EmirOguz <eemiroguzz@gmail.com>
2024-03-08 11:46:47 +0300

    basket index

--- src/Web/Controllers/BasketController.cs
+++ src/Web/Controllers/BasketController.cs
@@ -12,11 +12,24 @@ namespace Web.Controllers
             _basketViewModelService = basketViewModelService;
         }
 
+        public async Task<IActionResult> Index()
+        {
+            var basketViewModel = await _basketViewModelService.GetBasketViewModelAsync();
+            return View(basketViewModel);
+        }
+
+
         [HttpPost]
         public async Task<IActionResult> AddItem(int productId, int quantity = 1)
         {
             var basket = await _basketViewModelService.AddItemToBasketAsync(productId, quantity);
             return Json(basket);
+        }
+
+        public async Task<IActionResult> Empty()
+        {
+            var basketViewModel = await _basketViewModelService.GetBasketViewModelAsync();
+            return View(basketViewModel);
         }
     }
 }
